1d14e660204f3921542310664f9fa68f448f6519
KingPhilip14 <114031398+KingPhilip14@users.noreply.github.com>
2023-02-23 17:48:12 -0600

    Action type (#1)
    * Properly formatted the enums.py file while adding the ActionType enum class. All classes now inherit from Enum.
    
    * Update enums.py
    
    Properly formatted the enums, and every class inherits from the Enum class.

--- game/common/enums.py
+++ game/common/enums.py
@@ -1,11 +1,30 @@
-class DebugLevel:
-    none = 0
-    client = 1
-    controller = 2
-    engine = 3
+from enum import Enum, auto
 
 
-class ObjectType:
-    none = 0
-    action = 1
-    player = 2
+class DebugLevel(Enum):
+    NONE = auto()
+    CLIENT = auto()
+    CONTROLLER = auto()
+    ENGINE = auto()
+
+
+class ObjectType(Enum):
+    NONE = auto()
+    ACTION = auto()
+    PLAYER = auto()
+    GAMEBOARD = auto()
+    TILE = auto()
+    ITEM = auto()
+
+
+class ActionType(Enum):
+    NONE = auto()
+    MOVE_UP = auto()
+    MOVE_DOWN = auto()
+    MOVE_LEFT = auto()
+    MOVE_RIGHT = auto()
+    INTERACT_UP = auto()
+    INTERACT_DOWN = auto()
+    INTERACT_LEFT = auto()
+    INTERACT_RIGHT = auto()
+    INTERACT_CENTER = auto()
