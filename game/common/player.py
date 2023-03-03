8de2b6307c73c3625781af83f5cc0d1de7d6bcb6
KingPhilip14 <114031398+KingPhilip14@users.noreply.github.com>
2023-03-02 18:42:55 -0600

    Added avatar implementations into the player.py file (#11)
    * Added avatar implementations into the player.py file
    
    Imported the Avatar class and added getter and setter methods for all the fields of the class. To and From json methods now include avatar as well.
    
    * Changed constructor method
    
    Added type hinting into the constructor method.

--- game/common/player.py
+++ game/common/player.py
@@ -1,20 +1,64 @@
-import uuid
-
 from game.common.action import Action
 from game.common.game_object import GameObject
+from game.common.avatar import Avatar
 from game.common.enums import *
 
 
 class Player(GameObject):
-    def __init__(self, code=None, team_name=None, action=None):
+    def __init__(self, code: object = None, team_name: str = None, action: Action = None, avatar: Avatar = None):
         super().__init__()
-        self.object_type = ObjectType.player
-        
-        self.functional = True
-        self.error = None
-        self.team_name = team_name
-        self.code = code
-        self.action = action
+        self.object_type: ObjectType = ObjectType.PLAYER
+        self.functional: bool = True
+        self.error: object = None
+        self.team_name: str = team_name
+        self.code: object = code
+        self.action: Action = action
+        self.avatar: Avatar = avatar
+
+    @property
+    def action(self) -> Action:  # Will need to change Avatar class to the enum eventually
+        return self.__action
+
+    @action.setter
+    def action(self, action: Action):
+        if action is None or isinstance(action, Action):
+            self.__action = action
+
+    @property
+    def functional(self) -> bool:
+        return self.__functional
+
+    @functional.setter
+    def functional(self, functional: bool):
+        if functional is None or isinstance(functional, bool):
+            self.__functional = functional
+
+    @property
+    def team_name(self) -> str:
+        return self.__team_name
+
+    @team_name.setter
+    def team_name(self, team_name: str):
+        if team_name is None or isinstance(team_name, str):
+            self.__team_name = team_name
+
+    @property
+    def avatar(self) -> Avatar:
+        return self.__avatar
+
+    @avatar.setter
+    def avatar(self, avatar: Avatar):
+        if avatar is None or isinstance(avatar, Avatar):
+            self.__avatar = avatar
+
+    @property
+    def object_type(self) -> ObjectType:
+        return self.object_type
+
+    @object_type.setter
+    def object_type(self, object_type: ObjectType):
+        if object_type is None or isinstance(object_type, GameObject):
+            self.__object_type = object_type
 
     def to_json(self):
         data = super().to_json()
@@ -23,6 +67,7 @@ class Player(GameObject):
         data['error'] = self.error
         data['team_name'] = self.team_name
         data['action'] = self.action.to_json() if self.action is not None else None
+        data['avatar'] = self.avatar.to_json() if self.avatar is not None else None
 
         return data
 
@@ -32,8 +77,8 @@ class Player(GameObject):
         self.functional = data['functional']
         self.error = data['error']
         self.team_name = data['team_name']
-        act = Action()
-        self.action = act.from_json(data['action']) if data['action'] is not None else None
+        self.action = Action().from_json(data['action']) if data['action'] is not None else None
+        self.avatar = Avatar().from_json(data['avatar']) if data['avatar'] is not None else None
 
     def __str__(self):
         p = f"""ID: {self.id}
