d358bb36b809ce791f6dcb31faeb5d4e5d4539cd
MyFridgeIsFinePleaseStopAsking <101206316+JuliaCaesar@users.noreply.github.com>
2023-05-22 22:37:42 -0500

    Create active_ability.py

---
+++ game/quarry_rush/active_ability.py
@@ -0,0 +1,10 @@
+# I have no idea what i need to import for this yet, so i imported everything, naturally
+from game.common.map import *
+from game.common.items import *
+from game.common.stations import *
+from game.controllers import *
+
+
+class ActiveAbility:
+
+    def __init__(self):
