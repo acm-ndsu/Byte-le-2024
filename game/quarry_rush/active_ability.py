7d5567398551ec8acd965ad6435398223576d1d1
MyFridgeIsFinePleaseStopAsking <101206316+JuliaCaesar@users.noreply.github.com>
2023-05-23 17:28:18 -0500

    Update active_ability.py

--- game/quarry_rush/active_ability.py
+++ game/quarry_rush/active_ability.py
@@ -1,10 +1,37 @@
-# I have no idea what i need to import for this yet, so i imported everything, naturally
-from game.common.map import *
-from game.common.items import *
-from game.common.stations import *
-from game.controllers import *
+from game.common.enums import ObjectType
+from game.common.game_object import GameObject
 
 
-class ActiveAbility:
+class ActiveAbility(GameObject):
 
-    def __init__(self):
+    def __init__(self, name: str, cooldown: int):
+        super().__init__()
+        self.object_type = ObjectType
+        self.name: str = name
+        self.cooldown: int = cooldown
+
+# name getter
+    @property
+    def name(self) -> str:
+        return self.__name
+
+# name setter
+    @name.setter
+    def name(self, name: str) -> "":
+        if name is not isinstance(name, str):
+            raise ValueError(f'{self.__class__.__name__}.name must be a String')
+        self.__name = name
+
+# The cooldown represents the amount of turns that the ability is unavailable.
+# cooldown getter
+    @property
+    def cooldown(self) -> int:
+        return self.__cooldown
+
+# cooldown setter
+    @cooldown.setter
+    def cooldown(self, cooldown: int) -> 1:   # currently I just have the default set to 1, should not be negative
+        if cooldown is not isinstance(cooldown, int):
+            raise ValueError(f'{self.__class__.__name__}.cooldown must be an int')
+        self.__cooldown = cooldown
+
