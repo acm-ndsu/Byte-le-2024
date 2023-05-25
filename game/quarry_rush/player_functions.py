33461b9476544010fc5b983d437e6d32f41820ee
Brandon Gasser <34321725+brandongasser@users.noreply.github.com>
2023-05-25 13:40:58 -0500

    Tech tree (#2)
    * added tech tree interface
    
    * Added research functionality
    
    * bugfixes on tech tree
    
    * documentation and emp/trap detection
    
    * added tech info
    
    * added missing comment
    
    * tests for tech tree
    
    * trying to get ci working
    
    * Revert "trying to get ci working"
    
    This reverts commit 26aed6e1146a0f72488eae1a0b157cfe230656b0.
    
    * apply tech when researched
    
    * fixed failing tests
    
    * tech tree json
    
    * improved tech tree tests

---
+++ game/quarry_rush/player_functions.py
@@ -0,0 +1,48 @@
+from typing import Callable
+
+class PlayerFunctions:
+    """
+    This class is used as an interface for creating a tech tree
+
+    Attributes
+    ----------
+    * increase_movement: A function that takes an int for the amount to
+    increase movement speed by and increases the movement speed of the
+    player
+
+    * increase_mining: A function that takes a float for the amount to
+    increase mining drop rate by and increases the mining drop rate of
+    the player
+
+    * unlock_movement_overdrive: A function that unlocks the movement
+    overdrive ability for the player
+
+    * unlock_mining_overdrive: A function that unlocks the mining overdrive
+    ability for the player
+
+    * unlock_dynamite: A function that unlocks dynamite for the player
+
+    * unlock_landmines: A function that unlocks landmines for the player
+
+    * unlock_emps: A function that unlocks emps for the player
+
+    * unlock_trap_detection: A function that unlocks trap detection
+    for the player
+    """
+    def __init__(self,
+                 increase_movement: Callable[[int], None],
+                 increase_mining: Callable[[float], None],
+                 unlock_movement_overdrive: Callable[[], None],
+                 unlock_mining_overdrive: Callable[[], None],
+                 unlock_dynamite: Callable[[], None],
+                 unlock_landmines: Callable[[], None],
+                 unlock_emps: Callable[[], None],
+                 unlock_trap_detection: Callable[[], None]):
+        self.increase_movement = increase_movement
+        self.increase_mining = increase_mining
+        self.unlock_movement_overdrive = unlock_movement_overdrive
+        self.unlock_mining_overdrive = unlock_mining_overdrive
+        self.unlock_dynamite = unlock_dynamite
+        self.unlock_landmines = unlock_landmines
+        self.unlock_emps = unlock_emps
+        self.unlock_trap_detection = unlock_trap_detection
---
+++ game/test_suite/tests/test_tech_tree.py
@@ -0,0 +1,107 @@
+import unittest
+from unittest.mock import Mock
+
+from game.quarry_rush.player_functions import PlayerFunctions
+from game.quarry_rush.tech_tree import TechTree
+
+class TestTechTree(unittest.TestCase):
+    def setUp(self):
+        self.mock_increase_movement = Mock()
+        self.mock_increase_mining = Mock()
+        self.mock_unlock_movement_overdrive = Mock()
+        self.mock_unlock_mining_overdrive = Mock()
+        self.mock_unlock_dynamite = Mock()
+        self.mock_unlock_landmines = Mock()
+        self.mock_unlock_emps = Mock()
+        self.mock_unlock_trap_detection = Mock()
+        self.player_functions = PlayerFunctions(increase_movement=self.mock_increase_movement,
+                                                increase_mining=self.mock_increase_mining,
+                                                unlock_movement_overdrive=self.mock_unlock_movement_overdrive,
+                                                unlock_mining_overdrive=self.mock_unlock_mining_overdrive,
+                                                unlock_dynamite=self.mock_unlock_dynamite,
+                                                unlock_landmines=self.mock_unlock_landmines,
+                                                unlock_emps=self.mock_unlock_emps,
+                                                unlock_trap_detection=self.mock_unlock_trap_detection)
+        
+        self.tech_tree = TechTree(self.player_functions)
+        
+    def test_tech_names(self):
+        names = self.tech_tree.tech_names()
+        self.assertEqual(names, ['Mining Robotics', 'Better Drivetrains', 'Unnamed Drivetrain Tech', 'Overdrive Movement', 'High Yield Drilling', 'Unnamed Mining Tech', 'Overdrive Mining', 'Dynamite', 'Landmines', 'EMPs', 'Trap Detection'])
+        
+    def test_researched_techs(self):
+        names = self.tech_tree.researched_techs()
+        self.assertEqual(names, ['Mining Robotics'])
+        
+    def test_research(self):
+        result1 = self.tech_tree.research('Better Drivetrains')
+        result2 = self.tech_tree.research('Invalid Tech Name')
+        names = self.tech_tree.researched_techs()
+        self.assert_(result1)
+        self.assertFalse(result2)
+        self.assertEqual(names, ['Mining Robotics', 'Better Drivetrains'])
+        self.mock_increase_movement.assert_called_once_with(1)
+        
+    def test_illegal_research(self):
+        result = self.tech_tree.research('EMPs')
+        names = self.tech_tree.researched_techs()
+        self.assertFalse(result)
+        self.assertEqual(names, ['Mining Robotics'])
+        self.mock_unlock_emps.assert_not_called()
+        
+    def test_research_emp_with_detection(self):
+        self.tech_tree.research('High Yield Drilling')
+        self.tech_tree.research('Dynamite')
+        self.tech_tree.research('Landmines')
+        self.tech_tree.research('Trap Detection')
+        result = self.tech_tree.research('EMPs')
+        names = self.tech_tree.researched_techs()
+        self.assertFalse(result)
+        self.assertEqual(names, ['Mining Robotics', 'High Yield Drilling', 'Dynamite', 'Landmines', 'Trap Detection'])
+        self.mock_unlock_trap_detection.assert_called_once()
+        self.mock_unlock_emps.assert_not_called()
+        
+    def test_research_detection_with_emp(self):
+        self.tech_tree.research('High Yield Drilling')
+        self.tech_tree.research('Dynamite')
+        self.tech_tree.research('Landmines')
+        self.tech_tree.research('EMPs')
+        result = self.tech_tree.research('Trap Detection')
+        names = self.tech_tree.researched_techs()
+        self.assertFalse(result)
+        self.assertEqual(names, ['Mining Robotics', 'High Yield Drilling', 'Dynamite', 'Landmines', 'EMPs'])
+        self.mock_unlock_emps.assert_called_once()
+        self.mock_unlock_trap_detection.assert_not_called()
+        
+    def test_is_researched(self):
+        self.tech_tree.research('Better Drivetrains')
+        result1 = self.tech_tree.is_researched('Better Drivetrains')
+        result2 = self.tech_tree.is_researched('EMPs')
+        result3 = self.tech_tree.is_researched('Invalid Tech Name')
+        self.assert_(result1)
+        self.assertFalse(result2)
+        self.assertFalse(result3)
+        
+    def test_tech_info(self):
+        better_drivetrain_info = {'name': 'Better Drivetrains', 'cost': 0, 'point_value': 1}
+        result1 = self.tech_tree.tech_info('Better Drivetrains')
+        result2 = self.tech_tree.tech_info('Invalid Tech Name')
+        self.assertEqual(better_drivetrain_info['name'], result1.name)
+        self.assertEqual(better_drivetrain_info['cost'], result1.cost)
+        self.assertEqual(better_drivetrain_info['point_value'], result1.point_value)
+        self.assertIsNone(result2)
+        
+    def test_score(self):
+        self.tech_tree.research('Better Drivetrains')
+        self.tech_tree.research('High Yield Drilling')
+        self.tech_tree.research('Dynamite')
+        result = self.tech_tree.score()
+        self.assertEqual(result, 3)
+
+    def test_tech_tree_json(self):
+        self.tech_tree.research('High Yield Drilling')
+        self.tech_tree.research('Unnamed Mining Tech')
+        result = self.tech_tree.to_json()
+        for tech in self.tech_tree.tech_names():
+            self.assertEqual(result[tech], self.tech_tree.is_researched(tech))
+        self.assertEqual(self.player_functions, result['player_functions'])
