ddaccbe6c3328ed926a7c9cfb1b0726191f8e085
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2023-06-10 21:32:08 -0500

    Update ancient_tech.py

--- game/common/items/ancient_tech.py
+++ game/common/items/ancient_tech.py
@@ -7,7 +7,7 @@ from typing import Self
 
 class Ancient_Tech(Item):
     def __init__(self, science_point_value: int = 1, durability: int | None = None, quantity: int = 1, stack_size: int = 1, position: Vector | None = None, name: str | None = None):
-        super.__init__(None,science_point_value, durability,quantity,stack_size,position,name)
+        super.__init__(0,science_point_value, durability,quantity,stack_size,position,name)
         
 
     
