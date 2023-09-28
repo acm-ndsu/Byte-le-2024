0bd51366344c3d4a2f202545e8091383c927f6e2
Gunnar Moody <moodygman04@gmail.com>
2023-09-28 18:50:11 -0500

    OreOccupiableStations
    added:
    - stations for each ore type
    
    changed:
    - trap file name -> traps

--- game/quarry_rush/station/ore_occupiable_stations.py
+++ game/quarry_rush/station/ore_occupiable_stations.py
@@ -2,12 +2,16 @@ from game.common.avatar import Avatar
 from game.common.enums import ObjectType
 from game.common.items.item import Item
 from game.common.stations.occupiable_station import OccupiableStation
-from game.quarry_rush.entity.ores import Ore
+from game.quarry_rush.entity.ores import Ore, Lambdium, Turite, Copium
 from game.quarry_rush.avatar.inventory_manager import InventoryManager
 
 
 class OreOccupiableStation(OccupiableStation):
-    # MAKE 4 STATION CLASSES FOR EACH ORE OBJECT. THE TAKE ACTION METHOD WON'T CHANGE
+    """
+    Station that holds generic ore, inherits from OccupiableStation.
+
+    This station is inherited by other ore occupiable stations.
+    """
 
     def __init__(self, held_item: Ore | None = None):
         super().__init__(held_item=held_item)
@@ -21,3 +25,27 @@ class OreOccupiableStation(OccupiableStation):
         self.held_item = None
 
         return None
+
+
+class LambdiumOccupiableStation(OreOccupiableStation):
+    """
+    Occupiable Station that holds Lambdium ore.
+    """
+    def __init__(self, held_item: Lambdium | None = Lambdium()):
+        super().__init__(held_item=held_item)
+
+
+class TuriteOccupiableStation(OreOccupiableStation):
+    """
+    Occupiable Station that holds Turite ore.
+    """
+    def __init__(self, held_item: Turite | None = Turite()):
+        super().__init__(held_item=held_item)
+
+
+class CopiumOccupiableStation(OreOccupiableStation):
+    """
+    Occupiable Station that holds Copium ore.
+    """
+    def __init__(self, held_item: Copium | None = Copium()):
+        super().__init__(held_item=held_item)
