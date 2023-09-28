2364f5a1b6e0398896e7a7ffffe114202950812b
Gunnar Moody <115500822+MoodyMan04@users.noreply.github.com>
2023-09-28 18:18:40 -0500

    static game_board generation + more (#23)
    changes:
    - added game_location.json to gitignore
    - added inventory_manager to station.py
    - added two methods to inventory_manager.py:
        - cash_in_all: calls other two cash methods
        - is_empty: checks if first index is none, returns true if so, else otherwise
    - fixed issues with ancient_tech.py
    - fixed issues with ore.py
    - renamed game_map file to game_locations
    - added game_location.json with static locations for walls (not including the border), turing_bases, and church_bases
    - added game_location.py, which loads everything from game_location.json into a readable dict for game_board.locations
    - added company_station, which has a attribute for company and can call cash_in_all for the avatar if they share a company
    - fixed test files for ancient_tech and ore
    - added test file for company_station

--- game/quarry_rush/entity/ore.py
+++ game/quarry_rush/entity/ore.py
@@ -1,12 +1,10 @@
-from game.common.enums import ObjectType
-from game.common.game_object import GameObject
 from game.utils.vector import Vector
 from game.common.items.item import Item
-from typing import Self
 
 
 class Ore(Item):
-    def __init__(self, value: int = 1, durability: int | None = None, quantity: int = 1, stack_size: int = 1, position: Vector | None = None, name: str | None = None):
-        super.__init__(value,0, durability,quantity,stack_size,position,name)
-
-
+    """
+    Class for generic Ore item
+    """
+    def __init__(self, value: int = 1, quantity: int = 1, stack_size: int = 1, durability: int | None = None, position: Vector | None = None, name: str | None = None):
+        super().__init__(value, 0, quantity, stack_size, durability, position, name)
