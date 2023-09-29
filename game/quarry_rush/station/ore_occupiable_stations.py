fab0f5f7ae1e05276065f0ae3047d15e9984e3c0
Gunnar Moody <115500822+MoodyMan04@users.noreply.github.com>
2023-09-28 19:43:29 -0500

    ancient_tech_occupiable_stations (#26)
    added:
    - Trap enums (TRAP, LANDMINE, EMP)
    - AncientTechOccupiableStation
    
    chnaged:
    - fixed type hinting in ancient_tech_occupiable_station
    - removed return None in take_action of ancient_tech_occupiable_station

--- game/quarry_rush/station/ore_occupiable_stations.py
+++ game/quarry_rush/station/ore_occupiable_stations.py
@@ -13,25 +13,24 @@ class OreOccupiableStation(OccupiableStation):
     This station is inherited by other ore occupiable stations.
     """
 
-    def __init__(self, held_item: Ore | None = None):
+    def __init__(self, held_item: Ore = Ore()):
         super().__init__(held_item=held_item)
         self.object_type = ObjectType.ORE_OCCUPIABLE_STATION
 
-    def take_action(self, avatar: Avatar, inventory_manager: InventoryManager = None) -> Item | None:
+    def take_action(self, avatar: Avatar, inventory_manager: InventoryManager = None) -> None:
         if InventoryManager is None:
             raise ValueError(f'{self.__class__.__name__}.take_action() needs an InventoryManager Object.')
 
         inventory_manager.give(self.held_item, avatar.company)
         self.held_item = None
 
-        return None
-
 
 class LambdiumOccupiableStation(OreOccupiableStation):
     """
     Occupiable Station that holds Lambdium ore.
     """
-    def __init__(self, held_item: Lambdium | None = Lambdium()):
+
+    def __init__(self, held_item: Lambdium = Lambdium()):
         super().__init__(held_item=held_item)
         self.object_type = ObjectType.LAMBDIUM_OCCUPIABLE_STATION
 
@@ -40,7 +39,8 @@ class TuriteOccupiableStation(OreOccupiableStation):
     """
     Occupiable Station that holds Turite ore.
     """
-    def __init__(self, held_item: Turite | None = Turite()):
+
+    def __init__(self, held_item: Turite = Turite()):
         super().__init__(held_item=held_item)
         self.object_type = ObjectType.TURITE_OCCUPIABLE_STATION
 
@@ -49,6 +49,7 @@ class CopiumOccupiableStation(OreOccupiableStation):
     """
     Occupiable Station that holds Copium ore.
     """
-    def __init__(self, held_item: Copium | None = Copium()):
+
+    def __init__(self, held_item: Copium = Copium()):
         super().__init__(held_item=held_item)
         self.object_type = ObjectType.COPIUM_OCCUPIABLE_STATION
