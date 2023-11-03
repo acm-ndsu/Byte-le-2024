9069168475b15c776c43f1415d8b0ec935c324a4
Gunnar Moody <moodygman04@gmail.com>
2023-11-02 20:21:54 -0500

    changed game_location to 14x14
    changed:
    - game_location.json from 22x22 to 14x14 design
    - added blank new line to vector.py
    - changed tests to reflect changes

--- game/test_suite/tests/test_game_location.py
+++ game/test_suite/tests/test_game_location.py
@@ -19,53 +19,24 @@ class TestGameLocation(unittest.TestCase):
     def test_game_locations(self) -> None:
         # Get dict for expected value for game locations
         expected: dict[tuple[Vector]: list[GameObject]] = {
-            (Vector(1, 1),): [Wall(), ], (Vector(4, 1),): [Wall(), ], (Vector(5, 1),): [Wall(), ],
-            (Vector(20, 1),): [Wall(), ],
-            (Vector(4, 2),): [Wall(), ], (Vector(4, 3),): [Wall(), ], (Vector(4, 4),): [Wall(), ],
-            (Vector(9, 4),): [Wall(), ],
-            (Vector(10, 4),): [Wall(), ], (Vector(11, 4),): [Wall(), ], (Vector(12, 4),): [Wall(), ],
-            (Vector(13, 4),): [Wall(), ],
-            (Vector(14, 4),): [Wall(), ], (Vector(15, 4),): [Wall(), ], (Vector(16, 4),): [Wall(), ],
-            (Vector(4, 5),): [Wall(), ],
-            (Vector(8, 5),): [Wall(), ], (Vector(9, 5),): [Wall(), ], (Vector(16, 5),): [Wall(), ],
-            (Vector(17, 5),): [Wall(), ],
-            (Vector(4, 6),): [Wall(), ], (Vector(8, 6),): [Wall(), ], (Vector(17, 6),): [Wall(), ],
-            (Vector(4, 7),): [Wall(), ],
-            (Vector(8, 7),): [Wall(), ], (Vector(17, 7),): [Wall(), ], (Vector(4, 8),): [Wall(), ],
-            (Vector(17, 8),): [Wall(), ],
-            (Vector(4, 9),): [Wall(), ], (Vector(17, 9),): [Wall(), ], (Vector(4, 10),): [Wall(), ],
-            (Vector(17, 10),): [Wall(), ],
-            (Vector(4, 11),): [Wall(), ], (Vector(17, 11),): [Wall(), ], (Vector(4, 12),): [Wall(), ],
-            (Vector(17, 12),): [Wall(), ],
-            (Vector(4, 13),): [Wall(), ], (Vector(17, 13),): [Wall(), ], (Vector(4, 14),): [Wall(), ],
-            (Vector(13, 14),): [Wall(), ],
-            (Vector(17, 14),): [Wall(), ], (Vector(4, 15),): [Wall(), ], (Vector(13, 15),): [Wall(), ],
-            (Vector(17, 15),): [Wall(), ],
-            (Vector(4, 16),): [Wall(), ], (Vector(5, 16),): [Wall(), ], (Vector(12, 16),): [Wall(), ],
-            (Vector(13, 16),): [Wall(), ],
-            (Vector(17, 16),): [Wall(), ], (Vector(5, 17),): [Wall(), ], (Vector(6, 17),): [Wall(), ],
-            (Vector(7, 17),): [Wall(), ],
-            (Vector(8, 17),): [Wall(), ], (Vector(9, 17),): [Wall(), ], (Vector(10, 17),): [Wall(), ],
-            (Vector(11, 17),): [Wall(), ],
-            (Vector(12, 17),): [Wall(), ], (Vector(17, 17),): [Wall(), ], (Vector(17, 18),): [Wall(), ],
-            (Vector(17, 19),): [Wall(), ],
-            (Vector(1, 20),): [Wall(), ], (Vector(16, 20),): [Wall(), ], (Vector(17, 20),): [Wall(), ],
-            (Vector(20, 20),): [Wall(), ],
-            (Vector(18, 18),): [TuringStation(), ], (Vector(19, 18),): [TuringStation(), ],
-            (Vector(20, 18),): [TuringStation(), ],
-            (Vector(18, 19),): [TuringStation(), ], (Vector(19, 19),): [TuringStation(), ],
-            (Vector(20, 19),): [TuringStation(), ],
-            (Vector(18, 20),): [TuringStation(), ], (Vector(19, 20),): [TuringStation(), ],
-            (Vector(2, 1),): [ChurchStation(), ], (Vector(3, 1),): [ChurchStation(), ],
-            (Vector(1, 2),): [ChurchStation(), ],
-            (Vector(2, 2),): [ChurchStation(), ], (Vector(3, 2),): [ChurchStation(), ],
-            (Vector(1, 3),): [ChurchStation(), ],
-            (Vector(2, 3),): [ChurchStation(), ], (Vector(3, 3),): [ChurchStation(), ]
+            (Vector(5, 1),): [Wall(), ], (Vector(6, 1),): [Wall(), ], (Vector(12, 1),): [Wall(), ],
+            (Vector(4, 2),): [Wall(), ], (Vector(5, 2),): [Wall(), ],
+            (Vector(3, 3),): [Wall(), ], (Vector(4, 3),): [Wall(), ], (Vector(9, 3),): [Wall(), ],
+            (Vector(3, 4),): [Wall(), ], (Vector(9, 4),): [Wall(), ], (Vector(10, 4),): [Wall(), ],
+            (Vector(3, 5),): [Wall(), ], (Vector(10, 5),): [Wall(), ],
+            (Vector(3, 6),): [Wall(), ], (Vector(10, 6),): [Wall(), ],
+            (Vector(3, 7),): [Wall(), ], (Vector(10, 7),): [Wall(), ],
+            (Vector(3, 8),): [Wall(), ], (Vector(10, 8),): [Wall(), ],
+            (Vector(3, 9),): [Wall(), ], (Vector(4, 9),): [Wall(), ], (Vector(10, 9),): [Wall(), ],
+            (Vector(4, 10),): [Wall(), ], (Vector(9, 10),): [Wall(), ], (Vector(10, 10),): [Wall(), ],
+            (Vector(8, 11),): [Wall(), ], (Vector(9, 11),): [Wall(), ],
+            (Vector(1, 12),): [Wall(), ], (Vector(7, 12),): [Wall(), ], (Vector(8, 12),): [Wall(), ],
+            (Vector(9, 12),): [TuringStation(), ],
+            (Vector(4, 1),): [ChurchStation(), ]
         }
 
         # Get the actual result by calling the method
         actual: dict[tuple[Vector], list[GameObject]] = self.game_location.generate_location()
-
         # compare all values within the dict to see if they match
         for (x, y), (i, j) in zip(expected.items(), actual.items()):
             for ake, bke in zip(x, i):
