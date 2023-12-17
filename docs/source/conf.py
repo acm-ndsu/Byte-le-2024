3b60c642d26a95e35c73efe92d4e70ebd42358c5
KingPhilip14 <kingian404@gmail.com>
2023-12-17 16:52:00 -0600

    Initial setup
    Changed the previous doc folder name to be 'engine_docs' to separate it from the game documentation. Initial set up with new docs folder started.

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Byte-le Royale 2024'
+copyright = '1974-2023, NDSU ACM'
+author = 'NDSU ACM'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'press'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Byte-le Royale 2024 documentation master file, created by
+   sphinx-quickstart on Sun Dec 17 16:17:54 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Byte-le Royale 2024's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
