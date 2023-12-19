d60b039056aede3532e886f94e118fd1a46e59be
Jean A. Eckelberg <fyrelynx11@gmail.com>
2023-12-18 20:05:33 -0600

    Added changes to test and generator to speed up generation and pass tests

--- game/quarry_rush/map/collectable/collectable_generator.py
+++ game/quarry_rush/map/collectable/collectable_generator.py
@@ -2,69 +2,63 @@ from json import load
 from game.utils.vector import Vector
 from game.quarry_rush.station.ore_occupiable_station import OreOccupiableStation
 import random as rand
-from typing import TypeAlias
+import numpy as np
 from perlin_noise import PerlinNoise
 
 
 class CollectableGenerator:
-    __board_size = 14  # This includes the borders. Field is 20x20
+    __board_size = 14  # This includes the borders. Field is 12x12
     __ore_count = 100
 
     def __init__(self, seed: int = rand.randint(0, 8675309)):
-        f = open('game/quarry_rush/map/collectable/collectable_weights.json')
-        collectable_weights = load(f)
-        f.close()
+        with open('game/quarry_rush/map/collectable/collectable_weights.json') as f:
+            collectable_weights = load(f)
 
         self.__ore_weights = collectable_weights['ore']
         self.__special_weights = collectable_weights['special']
         self.__ancient_tech_weights = collectable_weights['ancient_tech']
         self.__seed = seed
-        
-    def generate(self) -> dict[tuple[Vector], OreOccupiableStation]:
+
+    def generate(self) -> dict[tuple[Vector], list[OreOccupiableStation]]:
         noise_map = self.adjust(self.layer(self.__ore_weights, self.generate_perlin_noise()))
         threshold = sorted([x for row in noise_map for x in row])[-self.__ore_count]
         ore_map = self.map_threshold(threshold, noise_map)
-        
-        result: dict[tuple[Vector], OreOccupiableStation] = {}
-        for y in range(len(ore_map)):
-            for x in range(len(ore_map[y])):
-                if ore_map[y][x]:
-                    vec = Vector(x=x, y=y)
-                    result[(vec,)] = [ OreOccupiableStation(position=vec,
-                                                          seed=self.__seed,
-                                                          special_weight=self.__special_weights[y][x],
-                                                          ancient_tech_weight=self.__ancient_tech_weights[y][x]), ]
-        return result
-    
+
+        def make_ore_station(x: int, y: int) -> list[OreOccupiableStation]:
+            return [OreOccupiableStation(position=Vector(x=x, y=y),
+                                         seed=self.__seed,
+                                         special_weight=self.__special_weights[y][x],
+                                         ancient_tech_weight=self.__ancient_tech_weights[y][x]), ]
+
+        return {(Vector(x=x, y=y),): make_ore_station(x, y)
+                for y, ore_row in enumerate(ore_map)
+                for x, ore in enumerate(ore_row) if ore}
 
     def generate_random_noise(self) -> list[list[float]]:
-        rand.seed(self.__seed)
-        raw = [[rand.random() for x in range(self.__board_size)] for y in range(self.__board_size)]
-        return self.adjust(raw)
+        rng = np.random.default_rng(seed=self.__seed)
+        return self.adjust(rng.random((self.__board_size,) * 2).tolist())
 
     def generate_perlin_noise(self) -> list[list[float]]:
         noise = PerlinNoise(octaves=8, seed=self.__seed)
-        return self.adjust(
-            [[noise([x / self.__board_size, y / self.__board_size]) for x in range(self.__board_size)] for y in range(self.__board_size)])
+        func = np.vectorize(lambda y, x: noise([x / self.__board_size, y / self.__board_size]))
+        return self.adjust(np.fromfunction(func, (self.__board_size,) * 2).tolist())
 
     def adjust(self, raw: list[list[float]]) -> list[list[float]]:
         """
         Adjusts a weight map to be on the range [0..1]. This will always include 0 and 1 and maintain proportions
         """
-        minimum = min(map(min, raw))
-        maximum = max(map(max, raw))
-        mapper = lambda x: (1 / (maximum - minimum)) * (x - minimum)
-        return list(map(lambda xs: list(map(mapper, xs)), raw))
+        x = np.array(raw)
+        return ((x - x.min()) / (x.max() - x.min())).tolist()
 
     def layer(self, layer1: list[list[float]], layer2: list[list[float]]) -> list[list[float]]:
         """
         Layers two weight maps on top of each other by multiplying each index individually
         """
-        return [[layer1[y][x] * layer2[y][x] for x in range(self.__board_size)] for y in range(self.__board_size)]
+        return (np.array(layer1) * np.array(layer2)).tolist()
 
     def map_threshold(self, threshold: float, weight_map: list[list[float]]) -> list[list[bool]]:
         """
         Takes a weight map and returns a new map of booleans where the value is True if the
         value in the original map is >= the threshold
         """
-        return list(map(lambda xs: list(map(lambda x: x >= threshold, xs)), weight_map))
+        return (np.array(weight_map) >= threshold).tolist()
