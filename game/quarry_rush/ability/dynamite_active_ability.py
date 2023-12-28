7396c63cc32e274af9394094eb19990beaa70b8a
KingPhilip14 <kingian404@gmail.com>
2023-12-27 23:48:21 -0600

    Balancing
    Added values to the already existing active abilities to add basic balancing

--- game/quarry_rush/ability/dynamite_active_ability.py
+++ game/quarry_rush/ability/dynamite_active_ability.py
@@ -5,6 +5,8 @@ from typing import Self
 
 class DynamiteActiveAbility(ActiveAbility):
 
-    def __init__(self, cooldown: int = 1, fuse: int = 0):
+    def __init__(self, cooldown: int = 4, fuse: int = 0):
         super().__init__()
         self.object_type = ObjectType.DYNAMITE_ACTIVE_ABILITY
+        self.cooldown = cooldown
+        self.fuse = fuse  # default = 0 to be available right after purchase
--- game/quarry_rush/ability/landmine_active_ability.py
+++ game/quarry_rush/ability/landmine_active_ability.py
@@ -4,8 +4,8 @@ from game.common.enums import ObjectType
 
 class LandmineActiveAbility(ActiveAbility):
 
-    def __init__(self, cooldown: int = 1, fuse: int = 0):
+    def __init__(self, cooldown: int = 6, fuse: int = 0):
         super().__init__()
         self.object_type: ObjectType = ObjectType.LANDMINE_ACTIVE_ABILITY
         self.cooldown: int = cooldown
-        self.fuse: int = fuse
+        self.fuse: int = fuse  # default = 0 to be available right after purchase
