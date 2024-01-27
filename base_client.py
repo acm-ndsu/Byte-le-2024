ed69faa135f357a4e1610525a9e5251b9e66f248
Gunnar Moody <moodygman04@gmail.com>
2024-01-26 21:26:47 -0600

    refactoring base clint

--- base_client.py
+++ base_client.py
@@ -2,8 +2,6 @@ import random
 
 from game.client.user_client import UserClient
 from game.common.enums import *
-from game.quarry_rush.entity.placeable.dynamite import Dynamite
-from game.quarry_rush.entity.placeable.traps import Trap
 from game.utils.vector import Vector
 
 
@@ -24,17 +22,17 @@ class Client(UserClient):
         """
         return 'Volunteer'
 
-    def first_turn_init(self, world, mobbot):
+    def first_turn_init(self, world, avatar):
         """
         This is where you can put setup for things that should happen at the beginning of the first turn
         """
-        self.company = mobbot.company
+        self.company = avatar.company
         self.my_station_type = ObjectType.TURING_STATION if self.company == Company.TURING else ObjectType.CHURCH_STATION
         self.current_state = State.MINING
         self.base_position = world.get_objects(self.my_station_type)[0][0]
 
     # This is where your AI will decide what to do
-    def take_turn(self, turn, actions, world, mobbot):
+    def take_turn(self, turn, actions, world, avatar):
         """
         This is where your AI will decide what to do.
         :param turn:        The current turn of the game.
@@ -42,148 +40,38 @@ class Client(UserClient):
         :param world:       Generic world information
         """
         if turn == 1:
-            self.first_turn_init(world, mobbot)
+            self.first_turn_init(world, avatar)
 
-        if world.game_map[mobbot.position.y][mobbot.position.x].occupied_by.object_type == self.my_station_type:
-            tech_buy = self.determine_tech_buy(mobbot)
-            if len(tech_buy) > 0:
-                return tech_buy
-        if len(self.get_my_inventory(world)) > 40:
-            return self.find_path(mobbot.position, self.base_position, world)
+        current_tile = world.game_map[avatar.position.y][
+            avatar.position.x]  # set current tile to the tile that I'm standing on
 
-        if world.game_map[mobbot.position.y][mobbot.position.x].occupied_by.object_type == ObjectType.ORE_OCCUPIABLE_STATION:
-            return [ActionType.MINE]
-        elif (mobbot.can_place_dynamite() and not
-                world.game_map[mobbot.position.y][mobbot.position.x].is_occupied_by_object_type(self.my_station_type) and not
-                world.game_map[mobbot.position.y][mobbot.position.x].is_occupied_by_object_type(ObjectType.DYNAMITE) and not
-                world.game_map[mobbot.position.y][mobbot.position.x].is_occupied_by_object_type(ObjectType.LANDMINE) and not
-                world.game_map[mobbot.position.y][mobbot.position.x].is_occupied_by_object_type(ObjectType.EMP)
-        ):
-            print('1')
-            return [ActionType.PLACE_DYNAMITE]
-        elif (mobbot.can_place_landmine() and not
-                world.game_map[mobbot.position.y][mobbot.position.x].is_occupied_by_object_type(ObjectType.LANDMINE)):
-            print('2')
-            return [ActionType.PLACE_LANDMINE]
-        elif (mobbot.can_place_emp() and not
-                world.game_map[mobbot.position.y][mobbot.position.x].is_occupied_by_object_type(ObjectType.EMP) and not
-                world.game_map[mobbot.position.y][mobbot.position.x].is_occupied_by_object_type(ObjectType.LANDMINE)):
-            print('3')
-            return [ActionType.PLACE_EMP]
+        # If I start the turn on my station, I should...
+        if current_tile.occupied_by.object_type == self.my_station_type:
+            # buy Improved Mining tech if I can...
+            if avatar.science_points >= avatar.get_tech_info('Improved Drivetrain').cost and not avatar.is_researched(
+                    'Improved Drivetrain'):
+                return [ActionType.BUY_IMPROVED_DRIVETRAIN]
+            # otherwise set my state to mining
+            self.current_state = State.MINING
 
+        # If I have at least 5 items in my inventory, set my state to selling
+        if len([item for item in self.get_my_inventory(world) if item is not None]) >= 5:
+            self.current_state = State.SELLING
 
-
-        ore_poses = [pose for (pose, _) in world.get_objects(ObjectType.ORE_OCCUPIABLE_STATION)]
-        closest_ore_pose = sorted(ore_poses, key=lambda pose: self.distance(mobbot.position, pose, world))[0]
-        actions = self.find_path(mobbot.position, closest_ore_pose, world)
+        # Make action decision for this turn
+        if self.current_state == State.SELLING:
+            # actions = [ActionType.MOVE_LEFT if self.company == Company.TURING else ActionType.MOVE_RIGHT] # If I'm selling, move towards my base
+            actions = self.generate_moves(avatar.position, self.base_position, turn % 2 == 0)
+        else:
+            if current_tile.occupied_by.object_type == ObjectType.ORE_OCCUPIABLE_STATION:
+                # If I'm mining and I'm standing on an ore, mine it
+                actions = [ActionType.MINE]
+            else:
+                # If I'm mining and I'm not standing on an ore, move randomly
+                actions = [random.choice(
+                    [ActionType.MOVE_LEFT, ActionType.MOVE_RIGHT, ActionType.MOVE_UP, ActionType.MOVE_DOWN])]
 
         return actions
-
-    def determine_tech_buy(self, mobbot):
-        if not mobbot.is_researched('Improved Mining'):
-            info = mobbot.get_tech_info('Improved Mining')
-            if mobbot.science_points >= info.cost:
-                return [ActionType.BUY_IMPROVED_MINING]
-
-        elif not mobbot.is_researched('Improved Drivetrain'):
-            info = mobbot.get_tech_info('Improved Drivetrain')
-            if mobbot.science_points >= info.cost:
-                return [ActionType.BUY_IMPROVED_DRIVETRAIN]
-
-        elif not mobbot.is_researched('Superior Mining'):
-            info = mobbot.get_tech_info('Superior Mining')
-            if mobbot.science_points >= info.cost:
-                return [ActionType.BUY_SUPERIOR_MINING]
-
-        elif not mobbot.is_researched('Superior Drivetrain'):
-            info = mobbot.get_tech_info('Superior Drivetrain')
-            if mobbot.science_points >= info.cost:
-                return [ActionType.BUY_SUPERIOR_DRIVETRAIN]
-
-        elif not mobbot.is_researched('Dynamite'):
-            info = mobbot.get_tech_info('Dynamite')
-            if mobbot.science_points >= info.cost:
-                return [ActionType.BUY_DYNAMITE]
-
-        elif not mobbot.is_researched('Landmines'):
-            info = mobbot.get_tech_info('Landmines')
-            if mobbot.science_points >= info.cost:
-                return [ActionType.BUY_LANDMINES]
-
-        elif not mobbot.is_researched(Tech.EMPS):
-            info = mobbot.get_tech_info(Tech.EMPS)
-            if mobbot.science_points >= info.cost:
-                return [ActionType.BUY_EMPS]
-
-        return []
-
-    def distance(self, pos_1, pos_2, world):
-        return len(self.find_path(pos_1, pos_2, world))
-
-    def find_path(self, start, goal, world):
-        start = (start.y, start.x)
-        goal = (goal.y, goal.x)
-        h = lambda pos: abs(pos[0] - goal[0]) + abs(pos[1] - goal[1])
-
-        def reconstruct_path(came_through, current):
-            total_path = [current]
-            while current in came_through.keys():
-                current = came_through[current]
-                total_path = [current] + total_path
-            return total_path
-
-        def to_actions(poses):
-            result = []
-            for i in range(len(poses) - 1):
-                if poses[i + 1][0] > poses[i][0]:
-                    result.append(ActionType.MOVE_DOWN)
-                if poses[i + 1][0] < poses[i][0]:
-                    result.append(ActionType.MOVE_UP)
-                if poses[i + 1][1] > poses[i][1]:
-                    result.append(ActionType.MOVE_RIGHT)
-                if poses[i + 1][1] < poses[i][1]:
-                    result.append(ActionType.MOVE_LEFT)
-            return result
-
-        def neighbors(pos):
-            return list(filter(lambda p: p[0] in range(0, 14) and p[1] in range(0, 14) and (
-                    world.game_map[pos[0]][pos[1]].occupied_by is None or world.game_map[pos[0]][
-                pos[1]].occupied_by.object_type != ObjectType.WALL),
-                               [(pos[0] + 1, pos[1]), (pos[0] - 1, pos[1]), (pos[0], pos[1] + 1),
-                                (pos[0], pos[1] - 1)]))
-
-        looking_set = {start}
-
-        came_through = {}
-
-        g_score = {}
-        g_score[start] = 0
-
-        f_score = {}
-        f_score[start] = h(start)
-
-        def get_f_score(pos):
-            return f_score[pos] if pos in f_score.keys() else 999999999  # f score is infinity if it isn't already set
-
-        while len(looking_set) != 0:
-            current = None
-            for pos in looking_set:
-                if current is None or get_f_score(pos) < get_f_score(current):
-                    current = pos
-            if current == goal:
-                return to_actions(reconstruct_path(came_through, current))
-
-            looking_set.remove(current)
-            for neighbor in neighbors(current):
-                tentative_g_score = g_score[current] + 1
-                if neighbor not in g_score.keys() or tentative_g_score < g_score[neighbor]:
-                    came_through[neighbor] = current
-                    g_score[neighbor] = tentative_g_score
-                    f_score[neighbor] = tentative_g_score + h(neighbor)
-                    if neighbor not in looking_set:
-                        looking_set.add(neighbor)
-
-        return []
 
     def generate_moves(self, start_position, end_position, vertical_first):
         """
@@ -203,4 +91,4 @@ class Client(UserClient):
         return vertical + horizontal if vertical_first else horizontal + vertical
 
     def get_my_inventory(self, world):
-        return [item for item in world.inventory_manager.get_inventory(self.company) if item is not None]
+        return world.inventory_manager.get_inventory(self.company)
